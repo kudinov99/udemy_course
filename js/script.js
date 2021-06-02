2c20922d23a4d8935d5b9da2f85c9cd7752e0c76
Daniil <texhnolyzed.soul@gmail.com>
2021-06-02 11:29:51 +0300

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('loh');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+  lastFilmBall = prompt('На сколько оцените его?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+personalMovieDB.movies[lastFilm] = lastFilmBall;
+console.log(personalMovieDB);
